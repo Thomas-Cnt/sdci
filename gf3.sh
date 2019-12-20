8808296624b1877198648c2898e76c935e05721d
Bobillot <37578379+Bobillot@users.noreply.github.com>
2019-12-20 10:28:42 +0100

    Update gf3.sh

--- gf3.sh
+++ gf3.sh
@@ -1,1 +1,1 @@
-node gateway.js --local_ip 10.0.0.255 --local_port 8484 --local_name gf3 --remote_ip 10.0.0.252 --remote_port 8181 --remote_name gwi & node device.js --local_ip 10.0.0.255 --local_port 9001 --local_name dev1 --remote_ip 10.0.0.255 --remote_port 8484 --remote_name gf3
+node gateway.js --local_ip 10.0.0.255 --local_port 8484 --local_name gf3 --remote_ip 10.0.0.252 --remote_port 8181 --remote_name gwi & node device.js --local_ip 10.0.0.255 --local_port 9001 --local_name dev31 --remote_ip 10.0.0.255 --remote_port 8484 --remote_name gf3

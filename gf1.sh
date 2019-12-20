837cfbc78df57ba74e8d028163bf359d7d97bd00
Bobillot <37578379+Bobillot@users.noreply.github.com>
2019-12-20 10:28:01 +0100

    Update gf1.sh

--- gf1.sh
+++ gf1.sh
@@ -1,1 +1,1 @@
-node gateway.js --local_ip 10.0.0.253 --local_port 8282 --local_name gf1 --remote_ip 10.0.0.252 --remote_port 8181 --remote_name gwi & node device.js --local_ip 10.0.0.253 --local_port 9001 --local_name dev1 --remote_ip 10.0.0.253 --remote_port 8282 --remote_name gf1 
+node gateway.js --local_ip 10.0.0.253 --local_port 8282 --local_name gf1 --remote_ip 10.0.0.252 --remote_port 8181 --remote_name gwi & node device.js --local_ip 10.0.0.253 --local_port 9001 --local_name dev11 --remote_ip 10.0.0.253 --remote_port 8282 --remote_name gf1 

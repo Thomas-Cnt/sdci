e4d5ee65d9704fcf01f82dad5cc519743ecf11e7
Bobillot <37578379+Bobillot@users.noreply.github.com>
2020-01-10 10:48:18 +0100

    Update gi.sh

--- gi.sh
+++ gi.sh
@@ -1,1 +1,1 @@
-node gateway.js --local_ip 10.0.0.252 --local_port 8181 --local_name gwi --remote_ip 10.0.0.251 --remote_port 8080 --remote_name srv 
+sleep 3 & node gateway.js --local_ip 10.0.0.201 --local_port 8181 --local_name gwi --remote_ip 10.0.0.200 --remote_port 8080 --remote_name srv 
